bdb441ef89d9b165c6e50dccf6a77e80e7ed7f28
kmohmd <103967650+kmohmd@users.noreply.github.com>
2022-05-17 13:52:24 +0100

    Update piratescript.js

--- piratescript.js
+++ piratescript.js
@@ -1,6 +1,30 @@
 
 function piratize(input){
 
+    
+function piratize(input){
+    return this. first Name + this.last Name
+}
+get Full Name:()  user. last Name
     // add you code in here
     
-} 
+} function makeCounter(){
+    let count=0//set intial count to 0
+
+    return{
+        increment:function({
+            count+=1    // increment it by one when function is called
+            return count //reference the outer 'count'
+
+
+            }
+        }
+
+        )
+    }
+
+    let counter=make Counter():
+    Console.log(counter.increment()wil output1
+    console .log (counter.increment ()) will output2
+    console . log(count)             //Reference Error doesnt exist here
+     
